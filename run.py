86b65dd85221891a30849e456fbd67ebf7d8a66e
兮尘 <761516186@qq.com>
2020-10-10 16:37:19 +0800

    Update run.py

--- run.py
+++ run.py
@@ -85,14 +85,14 @@ class run():
         # Set training dataloader iterator
         train_dataloader_head = DataLoader(
             TrainDataset(train_triples, self.nentity, self.nrelation, round(len(train_triples)*0.1), 'head-batch'),
-            batch_size=200,
+            batch_size=50,
             shuffle=False,
             num_workers=0,
             collate_fn=TrainDataset.collate_fn
         )
         train_dataloader_tail = DataLoader(
             TrainDataset(train_triples, self.nentity, self.nrelation, round(len(train_triples)*0.1), 'tail-batch'),
-            batch_size=200,
+            batch_size=50,
             shuffle=False,
             num_workers=0,
             collate_fn=TrainDataset.collate_fn
